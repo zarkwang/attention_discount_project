fcbc5cca801b276fa5dfb4df4e96f6273f719b9d
ZarkWang <zijianwang1996@gmail.com>
2023-04-13 06:05:10 +0100

    chavez data analysis

--- model/mpl/cross_valid.py
+++ model/mpl/cross_valid.py
@@ -96,43 +96,48 @@ class KFvalidation:
         
         n_style = len(self._style)
         n_cv = len(self._cv)
-
-        self.n_fit = n_cv*n_style
         self.fit_list = list(itertools.product(np.arange(n_style), np.arange(n_cv)))
+        self.n_fit = len(self.fit_list)
 
     
     def fit(self):
         
         if self.n_jobs==0:
-            self.val_styles,self.val_params,self.val_scores = self.KFvalidation_SingleProcess()
+            self.val_styles,self.val_params,self.val_scores,self.success = self.KFvalidation_SingleProcess()
         else:
-            self.val_styles,self.val_params,self.val_scores = self.KFvalidation_MultiProcess(self.n_jobs)
+            self.val_styles,self.val_params,self.val_scores,self.success = self.KFvalidation_MultiProcess(self.n_jobs)
         
-        self.fails = (np.array(self.val_params,dtype=object) == estimation.config_param['msg']['fail_converge'])
 
 
     @staticmethod
     def train_val_func(style,train_set,val_set,n_max_attempt=10):
 
         train_result = estimation.config_param['msg']['fail_converge']
+        x0 = None
 
         for i in range(n_max_attempt):
-            output = estimation.mle(style=style,data=train_set)
+            mle = estimation.mle(style=style,data=train_set,x0=x0)
+            output = mle.solve()
             
             if output == estimation.config_param['msg']['fail_converge']:
-                print(estimation.config_param['msg']['fail_converge'],f'Attempt times: {i}.')
+                if isinstance(mle.params,np.ndarray):
+                    x0 = mle.params
+                else:
+                    x0 = None
+                print(f'Attempt times: {i+1}')
+                print(output)
                 continue 
             else:
                 train_result = output 
                 break
+        
+        params = mle.params
+        val_result = test_model(style=style,test_sample=val_set,params=mle.params)
+        val_scores = list(val_result.values())
 
-       
-        val_result = test_model(style=style,test_sample=val_set,params=train_result["params"])
-        s = style
-        p = [round(e,3) for e in train_result['params']]
-        v = [round(e,3) for e in list(val_result.values())]
-
-        return s,p,v
+        success = (train_result != estimation.config_param['msg']['fail_converge'])
+            
+        return params,val_scores,success
     
 
     def fit_func(self,i):
@@ -145,9 +150,10 @@ class KFvalidation:
         train_set = self._data[self._data.index.isin(self._cv[cv_id][0])]
         val_set = self._data[self._data.index.isin(self._cv[cv_id][1])]
 
-        s,p,v = self.train_val_func(input_style,train_set,val_set,n_max_attempt=self.n_max_attempt)
+        param,score,success = self.train_val_func(input_style,train_set,val_set,
+                                                        n_max_attempt=self.n_max_attempt)
 
-        return s,p,v
+        return input_style,param,score,success
 
     
     def KFvalidation_SingleProcess(self):
@@ -155,16 +161,18 @@ class KFvalidation:
         val_styles = []
         val_params = []
         val_scores = []
+        val_success = []
 
         for i in tqdm(self.fit_list):
 
-            s,p,v = self.fit_func(i)
+            input_style,param,score,success = self.fit_func(i)
 
-            val_styles.append(s)
-            val_params.append(p)
-            val_scores.append(v)
+            val_styles.append(input_style)
+            val_params.append(param)
+            val_scores.append(score)
+            val_success.append(success)
 
-        return val_styles,val_params,val_scores
+        return val_styles,val_params,val_scores,val_success
 
 
 
@@ -175,17 +183,21 @@ class KFvalidation:
             val_styles = []
             val_params = []
             val_scores = []
+            val_success = []
             
-            for s,p,v in tqdm(pool.imap(self.fit_func,range(self.n_fit)),total=self.n_fit):
+            for input_style,param,score,success in tqdm(
+                                                    pool.imap(self.fit_func,range(self.n_fit)),
+                                                    total=self.n_fit):
                 
-                val_styles.append(s)
-                val_params.append(p)
-                val_scores.append(v)
+                val_styles.append(input_style)
+                val_params.append(param)
+                val_scores.append(score)
+                val_success.append(success)
 
         pool.join()
         pool.close()
 
-        return val_styles,val_params,val_scores
+        return val_styles,val_params,val_scores,val_success
     
 
     def summary(self):
@@ -211,6 +223,53 @@ class KFvalidation:
 
    
 
+def fit_and_test(data: pd.DataFrame,
+                 style_list: list,
+                 label=None,group=None,
+                 train_size=.8,random_state=2023,n_jobs=4):
+    
+    X = data.drop(label,axis=1)
+    y = data[label]
+    groups = data[group]
+    score_list = ['mse', 'mae', 'log_loss','accuracy','pred_ll']
 
+
+    # Split the data into train sample and test sample 
+    # Train sample containts 80% of the participants, test sample contains the rest 
+    split = model_selection.GroupShuffleSplit(n_splits=1,train_size=train_size,random_state=random_state).split(X,y,groups)
+    train_index,test_index = list(split)[0]
+
+    train_sample = data[data.index.isin(train_index)]
+    test_sample = data[data.index.isin(test_index)]
+
+
+    # Fit the data with discounted utility and trade-off model
+    with mp.Pool(processes=n_jobs) as pool:
+                
+        func = partial(KFvalidation.train_val_func,train_set=train_sample,val_set=test_sample)
+                
+        fit_styles = []
+        fit_params = []
+        fit_scores = []
+                
+        for s,p,v in tqdm(pool.imap(func,style_list),total=len(style_list)):
+
+            fit_styles.append(s)
+            fit_params.append(p)
+            fit_scores.append(v)
+
+    pool.join()
+    pool.close() 
+
+    # Get the results
+    fit_result = pd.DataFrame({'dstyle': [x['dstyle'] for x in fit_styles],
+                                'ustyle': [x['ustyle'] for x in fit_styles],
+                                'params': fit_params,
+                                'scores': fit_scores})
+
+    fit_result[score_list] = fit_result.scores.apply(lambda x:pd.Series(x))
+    fit_result.drop('scores',axis=1).sort_values('log_loss')
+    
+    return fit_result
 
     
--- model/mpl/estimation.py
+++ model/mpl/estimation.py
@@ -11,126 +11,140 @@ with open('mpl/config_param.yaml', 'r') as f:
         config_param = yaml.safe_load(f)
 
 
-def constraint(params,bound):
-    cons = True
 
-    if bound:
-        cons = ((np.array(bound)[:,0] <= params) & (np.array(bound)[:,1] >= params)).prod()
+class mle:
 
-    return cons
-
-
-
-def objective(params, data, dstyle, ustyle, method="logit", 
-                intercept=False,
-                bounds=None,
-                regenerate_sample=True,
-                kwargs={}):
-
-    if not constraint(params,bounds):
-        return 1e10 
-    else:
-        temper = params[-1]
-        ss_t = data['ss_t'].values
-        ss_x = data['ss_x'].values
-        ll_t = data['ll_t'].values
-        ll_x = data['ll_x'].values
-        choice = data['choice'].values
+    def __init__(self,style,data,x0=None,bounds=None):
         
-        if not regenerate_sample:
-            p_choice_ll = choice_rule.choice_prob(ss_x, ss_t, ll_x, ll_t, 
-                                                  params, dstyle, ustyle, temper, 
-                                                  intercept=intercept, 
-                                                  method=method, 
-                                                  regenerate_sample=False, 
-                                                  simu_sample=kwargs['simu_sample'])
+        self._data = data
+        self._style = style
+        self.dstyle = style['dstyle']
+        self.ustyle = style['ustyle']
+        self.method = style['method']
+        self.intercept = style['intercept']
+
+        init_intercept = [0]
+        bound_intercept = [[-100,100]]
+
+        if x0 is None:
+            self._x0 = config_param['discount_func'][self.dstyle]["x0"] + \
+                config_param['utility_func'][self.ustyle]["x0"] + \
+                init_intercept*self.intercept + \
+                config_param['choice_prob']['temper']["x0"]
         else:
-            p_choice_ll = choice_rule.choice_prob(ss_x, ss_t, ll_x, ll_t, 
-                                                  params, dstyle, ustyle, temper, 
-                                                  intercept=intercept,
-                                                  method=method)
+            self._x0 = x0
         
-        p_choice_ll[np.where(p_choice_ll == 1)] = p_choice_ll[np.where(p_choice_ll == 1)] - 1e-8
-        p_choice_ll[np.where(p_choice_ll == 0)] = p_choice_ll[np.where(p_choice_ll == 0)] + 1e-8
-
-        loss = metrics.log_loss(choice,p_choice_ll)
-
-        return loss
-
-
-
-
-
-def mle(style,data,disp_output=False,disp_step=False,simu_size=1000):
-
-    dstyle = style['dstyle']
-    ustyle = style['ustyle']
-    method = style['method']
-    intercept = style['intercept']
-
-    init_intercept = [0]
-    bound_intercept = [[-100,100]]
-
-    x0 = config_param['discount_func'][dstyle]["x0"] + \
-         config_param['utility_func'][ustyle]["x0"] + \
-         init_intercept*intercept + \
-         config_param['choice_prob']['temper']["x0"]
-    
-    bounds = config_param['discount_func'][dstyle]["bound"] + \
-             config_param['utility_func'][ustyle]["bound"] + \
-             bound_intercept*intercept + \
-             config_param['choice_prob']['temper']["bound"]
-
-    if method == 'probit':
-        np.random.seed(2023)
-        regenerate_sample = False
-        kwargs = {'simu_sample': np.random.normal(size=len(data)*simu_size).reshape(len(data),simu_size)}
-
-        minimizer_kwargs = {"method": "L-BFGS-B", 
-                            "args": (data, dstyle, ustyle, method, intercept, bounds, 
-                                     regenerate_sample, kwargs)}
-    else:
-        minimizer_kwargs = {"method": "L-BFGS-B", 
-                            "args": (data, dstyle, ustyle, method, intercept, bounds)}
+        if bounds is None: 
+            self.bounds = config_param['discount_func'][self.dstyle]["bound"] + \
+                    config_param['utility_func'][self.ustyle]["bound"] + \
+                    bound_intercept*self.intercept + \
+                    config_param['choice_prob']['temper']["bound"]
+        else:
+            self.bounds = bounds
         
     
-    stepsize = np.array(bounds)[:,1]*0.1
 
-    solver = basinhopping(objective, x0, minimizer_kwargs=minimizer_kwargs, 
-                niter=1000, 
-                stepsize=stepsize, 
-                T=1.0,
-                niter_success = 100,
-                disp=disp_step)
-    
-    #print(solver)
-    
-    if solver.success:
-        result = solver.lowest_optimization_result
-        se = np.sqrt(np.diag(result.hess_inv.todense())) / np.sqrt(len(data))
-        log_loss = result.fun
-        aic = 2*len(x0)+2*log_loss*len(data)
-        bic = 2*np.log(len(data))*len(x0)+2*log_loss*len(data)
-        gradient = result.jac
+    @staticmethod
+    def constraint(params,bound):
+        cons = True
+
+        if bound:
+            cons = ((np.array(bound)[:,0] <= params) & (np.array(bound)[:,1] >= params)).prod()
+
+        return cons
 
 
-        output= {'model':dstyle+'-'+ustyle,
-                'params':[round(e,3) for e in result.x],
-                'se':[round(e,3) for e in se],
-                'gradient':[round(e,3) for e in gradient],
-                'log_loss':round(log_loss,3),
-                'aic':round(aic,3),
-                'bic':round(bic,3),
-                }
+    def objective(self,params,regenerate_sample=True,kwargs={}):
+
+        if not self.constraint(params,self.bounds):
+            return 1e10 
+        else:
+            temper = params[-1]
+            ss_t = self._data['ss_t'].values
+            ss_x = self._data['ss_x'].values
+            ll_t = self._data['ll_t'].values
+            ll_x = self._data['ll_x'].values
+            choice = self._data['choice'].values
+            
+            if not regenerate_sample:
+                p_choice_ll = choice_rule.choice_prob(ss_x, ss_t, ll_x, ll_t, params, 
+                                                    dstyle=self.dstyle, 
+                                                    ustyle=self.ustyle, 
+                                                    temper=temper, 
+                                                    intercept=self.intercept, 
+                                                    method=self.method, 
+                                                    regenerate_sample=False, 
+                                                    simu_sample=kwargs['simu_sample'])
+            else:
+                p_choice_ll = choice_rule.choice_prob(ss_x, ss_t, ll_x, ll_t, params,
+                                                    dstyle=self.dstyle, 
+                                                    ustyle=self.ustyle, 
+                                                    temper=temper, 
+                                                    intercept=self.intercept,
+                                                    method=self.method)
+            
+            p_choice_ll[np.where(p_choice_ll == 1)] = p_choice_ll[np.where(p_choice_ll == 1)] - 1e-8
+            p_choice_ll[np.where(p_choice_ll == 0)] = p_choice_ll[np.where(p_choice_ll == 0)] + 1e-8
+
+            loss = metrics.log_loss(choice,p_choice_ll)
+
+            return loss
+        
+        
+    def solve(self,niter=500,niter_sucess=50,
+              regenerate_sample=True,simu_size=1000,
+              disp_output=False,disp_step=False,):
+        
+        if self.method == 'probit':
+            np.random.seed(2023)
+            regenerate_sample = False
+            simu_sample = np.random.normal(size=len(self._data)*simu_size).reshape(len(self._data),simu_size)
+            kwargs = {'simu_sample':simu_sample}
+
+            minimizer_kwargs = {"method": "L-BFGS-B", 
+                                "args": (regenerate_sample, kwargs)}
+        else:
+            minimizer_kwargs = {"method": "L-BFGS-B"}
+            
+        #stepsize = np.array(self.bounds)[:,1]*0.1
+
+        solver = basinhopping(self.objective, self._x0, 
+                              minimizer_kwargs=minimizer_kwargs, 
+                              T=1.0,
+                              niter=niter, 
+                              stepsize=0.5, 
+                              niter_success=niter_sucess,
+                              disp=disp_step)
+        
+        self.solver = solver
+        #print(solver)
+        
+        if solver.success:
+            result = solver.lowest_optimization_result
+            self.params = result.x
+            self.se = np.sqrt(np.diag(result.hess_inv.todense())) / np.sqrt(len(self._data))
+            self.log_loss = result.fun
+            self.aic = 2*len(self._x0)+2*self.log_loss*len(self._data)
+            self.bic = 2*np.log(len(self._data))*len(self._x0)+2*self.log_loss*len(self._data)
+            self.gradient = result.jac
+
+            self.output= {'style':[self._style],
+                        'params':[round(e,3) for e in self.params],
+                        'se':[round(e,3) for e in self.se],
+                        'gradient':[round(e,3) for e in self.gradient],
+                        'log_loss':round(self.log_loss,3),
+                        'aic':round(self.aic,3),
+                        'bic':round(self.bic,3)
+                        }
+            
+        else:
+            self.output = config_param['msg']['fail_converge']
+            self.params = solver.lowest_optimization_result.x
         
         if disp_output:
-            print(output)
+                print(self.output)
 
-        return output
-    else:
-        output = config_param['msg']['fail_converge']
-        print(output)
-        return output
+        return self.output
 
     
 #if __name__ == "__main__":
